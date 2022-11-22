bfd7b04a5c2c9426ffdcf58f8dd8a26f79c96d19
MukIlnur <24mi@bk.ru>
2022-11-22 16:48:15 +0500

    Добавил пару примеров

--- HomeWork10-OTUS/Program.cs
+++ HomeWork10-OTUS/Program.cs
@@ -1,4 +1,5 @@
-﻿using static System.Environment;
+﻿using System.Text;
+using static System.Environment;
 namespace HomeWork10_OTUS
 {
     internal class Program
@@ -24,7 +25,7 @@ namespace HomeWork10_OTUS
             string[] customPath = new[] { userFolder, "OTUS", "NewFolder" };
             string dir = Path.Combine(customPath); // комбинирует названия директорий
            
-            Console.WriteLine($"Is exist? {Directory.Exists(dir)}"); // проверка на существование папки
+            //Console.WriteLine($"Is exist? {Directory.Exists(dir)}"); // проверка на существование папки
 
             //Directory.CreateDirectory(dir);
             //Console.WriteLine($"Is exist? {Directory.Exists(dir)}");
@@ -32,7 +33,7 @@ namespace HomeWork10_OTUS
             //Directory.Delete(dir,true); // удаление папки
             //Console.WriteLine($"Is exist? {Directory.Exists(dir)}");
 
-            var textFile = Path.Combine(dir,"Dummy2.txt");
+            var textFile = Path.Combine(dir,"Dummy.csv");
             //Console.WriteLine(textFile);
 
             //var textWriter = File.CreateText(textFile);
@@ -43,13 +44,84 @@ namespace HomeWork10_OTUS
 
             //File.Copy(textFile, textFile + ".docx"); // через пакет DocumentFormat.OpenXml
 
-            using (var textWriter = File.CreateText(textFile))
-            {
-                textWriter.WriteLine("Boogy, boogy!");
+            //using (var textWriter = File.CreateText(textFile))
+            //{
+            //    textWriter.WriteLine("Boogy, boogy!");
 
+            //}
+
+            //File.Delete(textFile + ".docx");
+            //Console.WriteLine(File.Exists(textFile + ".docx"));
+
+            //var textReader = File.OpenText(textFile);
+            //Console.WriteLine(textReader.ReadToEnd());
+
+            //string s = "SomeText";
+            //char[] chars = s.ToCharArray();
+
+            //using (var writer = new StreamWriter(textFile, true))
+            //{
+            //    writer.WriteLine(chars);
+            //}
+
+            //using (var reader = new StreamReader(textFile))
+            //{
+            //    while (!reader.EndOfStream)
+            //    {
+            //        var row = reader.ReadLine();
+            //        Console.WriteLine(row);
+            //    }
+            //}
+
+            var csv = ReadData(textFile);
+            //foreach (var row in csv)
+            //{
+            //    row[0] += "X";
+            //}
+
+            //WriteData(csv, textFile);
+
+            static List<string[]> ReadData(string filename)
+            {
+                var result = new List<string[]>();
+                try
+                {
+                    using (var reader = new StreamReader (filename))
+                    {
+                        while (!reader.EndOfStream)
+                        {
+                            var line = reader.ReadLine();
+                            var values = line?.Split(';');
+                            result.Add(values);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                return result;
             }
 
-
+            static void WriteData(List<string[]> data, string filename)
+            {
+                try
+                {
+                    using (var writer = new StreamWriter(filename))
+                    {
+                        foreach (var item in data)
+                        {
+                            writer.WriteLine(string.Join(";", item));
+                        }
+                        
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
         }
     }
 }
